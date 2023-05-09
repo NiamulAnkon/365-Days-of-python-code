4947d73683a52ecaee81d19cceedca4b88c2720f
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2023-05-09 17:27:35 +0600

    Add files via upload

---
+++ Day-2/main.py
@@ -0,0 +1,17 @@
+#Type casting
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+
+print(c + d)
+#This is comment
+#i am at day-2
+
+#Escpae sequance
+print("Hi i am doing 365\nDys of code")
