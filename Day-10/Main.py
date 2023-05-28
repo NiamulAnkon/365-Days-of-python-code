c34809f94d77c321c9b3ce71226a49de6596e616
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2023-05-28 17:50:48 +0600

    Day-10

---
+++ Day-10/Main.py
@@ -0,0 +1,11 @@
+dic ={
+  "Ankon": "Human",
+  "Spoom": "object"
+}
+
+print(dic["Ankon"])
+
+info = {'name':'Ankon', 'age':11, 'eligible':True}
+print(info)
+print(info.keys())
+print(info.values())
