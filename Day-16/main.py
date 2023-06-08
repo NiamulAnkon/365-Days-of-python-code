55303999e8ada0a39fc263122b6892cba7294b61
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2023-06-08 21:44:11 +0600

    Day-16

---
+++ Day-16/main.py
@@ -0,0 +1,7 @@
+#How import works 
+from math import sqrt, pi
+from math import pi, sqrt as s
+import math as math_builtin_python
+
+result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+print(result)  # Output: 3.0
