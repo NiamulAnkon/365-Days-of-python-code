efdec1f59a0a4eac2fb5a8b5e34294dc2ebc7419
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2023-07-27 20:50:56 +0600

    Day-54

---
+++ Day-54/main.py
@@ -0,0 +1,18 @@
+#Getters and Setters in Python
+class MyClass:
+  def __init__(self, value):
+      self._value = value
+  def show(self):
+    print(f"Value is {self._value}")
+  @property
+  def ten_value(self):
+      return 10* self._value
+  @ten_value.setter
+  def ten_value(self, new_value):
+    self._value = new_value /10
+    return 10* self._value
+
+obj = MyClass(10)
+obj.ten_value = 67
+print(obj.ten_value)
+obj.show()
