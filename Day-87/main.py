765b159bbfb857d30d05a2f4ac1073869c502cdd
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2023-09-13 19:42:28 +0600

    Day-87

---
+++ Day-87/main.py
@@ -0,0 +1,9 @@
+#Shutil Module in Python
+import shutil
+import os
+
+# shutil.copy("main.py", "main2.py")
+# shutil.copytree(".tutorial", "mytutorial")
+# shutil.move(".tutorial/file.file", "file.file")
+# shutil.rmtree("mytutorial")
+os.remove("file.file")
