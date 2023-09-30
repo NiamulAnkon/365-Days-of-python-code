0546723b330c30f01cae73af16e70a9b3c0bcc1a
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2023-09-30 20:35:11 +0600

    Day-104

---
+++ Day-104/main.py
@@ -0,0 +1,4 @@
+from instabot import Bot
+bot = Bot()
+bot.login(username = "ankon1109", password = "can't tell you for security reason")
+bot.upload_photo("files/Screenshot (26).png", caption = "True")
