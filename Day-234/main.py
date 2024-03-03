2d47412166f14d698e4eff64e9695c888677906e
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2024-03-03 19:49:45 +0600

    Day-234

---
+++ Day-234/main.py
@@ -0,0 +1,15 @@
+#problem 3
+# You are given a list of integers. Write a function to return the sum of all the integers in the list.
+
+# For example:
+
+# For the list [1, 2, 3, 4, 5], the function should return 15
+
+
+num_list = [1, 2, 3, 4, 5]
+
+def sum_of_list():
+    print(sum(num_list))
+
+
+sum_of_list()
