67df0b102ddcd36445930c7d3b98f69dc5154788
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2024-06-01 23:23:57 +0600

    Day-320

---
+++ Day-320/main.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = "D:/ankonFolder/365daysofcode/Day-320"
+destination_dir = "D:/ankonFolder/365daysofcode/Day-320"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+    
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+
+schedule.every().day.at("18:57").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
