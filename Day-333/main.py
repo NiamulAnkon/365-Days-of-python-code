74296cae728fb8deac0b81a7ec42f20a49412cb9
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2024-06-16 22:44:01 +0600

    Day-333

---
+++ Day-333/main.py
@@ -0,0 +1,10 @@
+class finding_max_value:
+    def find_max_value(self):
+        List = [553,10,33,64,96,49,61,23,7,5,49,6,5,2,78,76,5772,93,796,974,607,4509,68,10,0,17,77,447,71,26,99,45,70,48,31,21,29,6,8,9,13,51,58,6,8,25,34,98,75,77]
+        max_value = max(List)
+        print(max_value)
+
+
+if __name__ == "__main__":
+    print_value = finding_max_value()
+    print_value.find_max_value()
