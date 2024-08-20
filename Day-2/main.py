e08ee4b3c08611a324f10039c9f89e5314a4f78a
NiamulAnkon <121841552+NiamulAnkon@users.noreply.github.com>
2024-08-20 22:41:15 +0600

    Day-2

--- Day-2/main.py
+++ Day-2/main.py
@@ -1,17 +1,31 @@
-#Type casting
-a = "1"
-# a = 1
-b = "2"
-# b = 2
-print(int(a) + int(b))
+#!/bin/python3
 
-# Implicit TypeCasting
-c = 1.9
-d = 8
+import math
+import os
+import random
+import re
+import sys
 
-print(c + d)
-#This is comment
-#i am at day-2
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
 
-#Escpae sequance
-print("Hi i am doing 365\nDys of code")
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * (tip_percent / 100)
+    tax = meal_cost * (tax_percent / 100)
+    total_cost = meal_cost + tip + tax
+    rounded = round(total_cost)
+    print(rounded)
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
